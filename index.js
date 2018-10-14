95c5b965b60be4a56bec072462227d32cf7c98cc
braj011 <bhagyaraj01@gmail.com>
2018-10-14 15:24:48 +0100

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function produceDrivingRange(blockRange) {
+  return function(bottomBlock, topBlock) {
+    let distance  = parseInt(topBlock.slice(0,-2) - bottomBlock.slice(0,-2))
+    // since blocks are entered as 'XXth' - must delete the 'th'
+    // can slice on a string to get rid of this + change to a number
+    if (blockRange > distance) {
+      return `within range by ${blockRange - distance}`
+    } else {
+      return `${distance - blockRange} blocks out of range`
+    }
+  }
+}
+// when interpolating - need to use backticks
+// closure - needs to return another function 
+
+function produceTipCalculator(tipDecimal) {
+  return function(fare) {
+    return fare * tipDecimal 
+  }
+}
+
+function createDriver() {
+  let driverId = 0
+  return class { 
+    constructor(name) {  
+      // class is the function here?         
+      this.name = name
+      this.id = ++driverId
+    }
+  }
+  // no extra functionality needed here 
+}
