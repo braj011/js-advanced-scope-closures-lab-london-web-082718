909d163831d530af18b826cac3e62f2beaf4f6c9
Bhagya Raj <bhagyaraj01@gmail.com>
2019-01-03 13:55:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ function createDriver() {
   let driverId = 0
   return class {
     constructor(name) {
-      this.name = name 
+      this.name = name;
+      this.id = ++driverId
     }
+    
   }
 }
